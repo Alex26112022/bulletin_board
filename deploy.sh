882bd7141e01302ec5c535db39488d5157f8de49
Алексей Денисенко <AlexeyDenisenko2703@yandex.ru>
2024-10-07 17:51:14 +0300

    check CI

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-sudo docker-compose stop
-sudo docker system prune -a -f
-sudo docker-compose build
-sudo docker-compose up -d
-sudo docker-compose exec web python manage.py migrate --noinput
-sudo docker-compose exec web python manage.py collectstatic --noinput
+docker-compose stop
+docker system prune -a -f
+docker-compose build
+docker-compose up -d
+docker-compose exec web python manage.py migrate --noinput
+docker-compose web python manage.py collectstatic --noinput

86a3a4a6bac9fcbd259ba82c4f75e27e47d3730a
Алексей Денисенко <AlexeyDenisenko2703@yandex.ru>
2024-10-10 13:29:40 +0300

    add models Bulletin, Comment

---
+++ bulletins/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from bulletins.models import Bulletin, Comment
+
+admin.site.register(Bulletin)
+admin.site.register(Comment)
--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework_simplejwt',
     'users',
+    'bulletins',
 ]
 
 MIDDLEWARE = [
